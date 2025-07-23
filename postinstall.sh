4d237e216f68a01fc2567fc8091df8142215fc82
ewsgit <ewsgit@ewsgit.uk>
2025-07-24 00:17:05 +0100

    yes

--- postinstall.sh
+++ postinstall.sh
@@ -112,7 +112,7 @@ cd ~/.themeRepos || exit
 echo "----- Installing gtk & libadwaita theme"
 git clone git@github.com:vinceliuice/MacTahoe-gtk-theme.git
 
-cd MacTahoe-git-theme || exit
+cd ~/.themeRepos/MacTahoe-git-theme || exit
 
 ./install.sh -t pink -o solid
 ./install.sh -l -t pink -o solid
@@ -120,7 +120,7 @@ cd MacTahoe-git-theme || exit
 echo "----- Installing icon theme"
 git clone git@github.com:vinceliuice/MacTahoe-icon-theme.git
 
-cd MacTahoe-icon-theme || exit
+cd ~/.themeRepos/MacTahoe-icon-theme || exit
 
 ./install.sh -t pink -b
 
